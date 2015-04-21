207e880f94073bb4941d6243bb82883cd1b97c73
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-04-21 10:35:54 -0700

    secod spec failed

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,7 @@ describe('String#scrabble') do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble()).to(eq(1))
   end
+  it("returns a scrabble score for a word") do
+    expect("path".scrabble()).to(eq(9))
+  end
 end
