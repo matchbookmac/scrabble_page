f8c7a1571505e48f99ecb816f5fd7ec10cf186ec
Ian MacDonald & Sunny Zhang <student@epicodus.com>
2015-04-22 11:27:45 -0700

    Pass integration testing,

--- app.rb
+++ app.rb
@@ -1,2 +1,13 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/scrabble')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/word') do
+  @score = params.fetch('word').scrabble()
+  erb(:score)
+end
